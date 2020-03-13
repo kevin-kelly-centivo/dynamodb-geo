25b0d46132d0471283437b05b06ec1e179453b31
thekevshow <kevin.kelly@centivo.com>
2020-03-13 13:55:24 -0400

    testing updates

--- node-implementation/packages/test/index.js
+++ node-implementation/packages/test/index.js
@@ -1,4 +1,5 @@
-const s2Manager = require('@dyanmodb-geo-enhaned/geo-core/s2Manager');
+const s2 = require("nodes2ts");
+const s2Manager = require('@dyanmodb-geo-enhanced/geo-core/s2Manager');
 
 // buffalo
 // let lat = 42.8864;
@@ -7,4 +8,7 @@ const s2Manager = require('@dyanmodb-geo-enhaned/geo-core/s2Manager');
 // orachrd park
 let lat = 42.7577;
 let long = -78.7703;
+
+
 console.log(s2Manager.getBoundingBoxForRadiusQuery(lat, long, 100));
+console.log(s2Manager.findCellIds(new s2.S2LatLng.fromDegrees(lat, long)));
