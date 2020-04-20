fd5adb747c0eea75c37d7397b3b65baafa31daf3
thekevshow <kevin.kelly@centivo.com>
2020-04-19 20:44:40 -0400

    more work on converting required query client elements

---
+++ node-implementation/packages/s3-geo/geoQueryClient.js
@@ -0,0 +1,42 @@
+const aws = require('aws-sdk');
+aws.config.update({ region: process.env.AWS_REGION || 'us-east-1' });
+const ddb = new aws.DynamoDB.DocumentClient();
+
+/**
+ * A convenience method that executes the <code>queryRequests</code> 
+ *  and applies the <code>resultFilter</code> to the query results.
+ *
+ * @return an immutable collection of filtered items
+ */
+const execute = async (geoQueryRequest) => {
+    let tasks = [];
+    for (let query of geoQueryRequest.getQueryRequests()) {
+        tasks.push(executeQuery(query, geoQueryRequest.getResultFilter()));
+    }
+    let results = await Promise.all(tasks);
+    // TODO might have to make a consolidated results =[] foreach cresults.push(...result)
+    return results;
+}
+
+/**
+ * Executes the  query using the provided db client. The geo filter is applied to the results of the query.
+ *
+ * @param queryRequest the query to execute
+ * @param resultFilter ---> GeoFilter <Map <String, AttributeValue>>
+ * @return a collection of filtered result items
+ */
+const executeQuery = async (queryRequest, resultFilter) => {
+    let queryResult;
+    // List <Map <String, AttributeValue>>
+    let resultItems = [];
+
+    do {
+        queryResult = ddb.query(queryRequest);
+        // List <Map <String, AttributeValue>>
+        let items = queryResult.getItems();
+        // filter the results using the geo filter
+        let filteredItems = resultFilter.filter(items);
+        resultItems.push(...filteredItems);
+        queryRequest = queryRequest.withExclusiveStartKey(queryResult.getLastEvaluatedKey());
+    } while ((queryResult.getLastEvaluatedKey() != null));
+}
--- node-implementation/packages/s3-geo/geoQueryHelper.js
+++ node-implementation/packages/s3-geo/geoQueryHelper.js
@@ -65,6 +65,59 @@ const generateGeoProperties = (boundingBox, hashKeyLength) => {
     return queryRequests;
 }
 
+/**
+     * For the given <code>QueryRequest</code> query and the boundingBox, this method creates a collection of queries
+     * that are decorated with geo attributes to enable geo-spatial querying.
+     *
+     * @param query       the original query request --> QueryRequest
+     * @param boundingBox the bounding lat long rectangle of the geo query --> S2LatLngRect
+     * @param config      the config containing caller's geo config, example index name, etc. --> GeoConfig
+     * @param compositeKeyValue the value of the column that is used in the construction of the composite hash key(geoHashKey + someOtherColumnValue).
+     *                          This is needed when constructing queries that need a composite hash key.
+     *                          For eg. Fetch an item where lat/long is 23.78787, -70.6767 AND category = 'restaurants' --> Optional <String>
+     * @return queryRequests an immutable collection of <code>QueryRequest</code> that are now "geo enabled" --> List <QueryRequest>
+     */
+const generateGeoQueries = async (query, boundingBox, config, compositeKeyValue) => {
+    let outerRanges = getGeoHashRanges(boundingBox);
+    let queryRequests = [];
+    for (let outerRange of outerRanges) {
+        let geohashRanges = outerRange.trySplit(config.getGeoHashKeyLength(), s2Manager);
+        for (let range of geohashRanges) {
+            // make a copy of the query request to retain original query attributes like table name, etc.
+            let queryRequest = JSON.parse(JSON.stringify(query));
+            // generate the hash key for the global secondary index
+            let geohashKey = s2Manager.generateHashKey(range.getRangeMin(), config.getGeoHashKeyLength());
+            // Map <String, Condition>
+            let keyConditions = [{ string: 2, condition: 1.0 }];
+
+            // construct the hashKey condition --> Condition
+            let geoHashKeyCondition;
+            if (config.getHashKeyDecorator() != null && compositeKeyValue != null) {
+                let compositeHashKey = config.getHashKeyDecorator().get().decorate(compositeKeyValue.get(), geohashKey);
+                geoHashKeyCondition = new Condition().withComparisonOperator(ComparisonOperator.EQ)
+                    .withAttributeValueList(new AttributeValue().withS(compositeHashKey));
+            } else {
+                geoHashKeyCondition = new Condition().withComparisonOperator(ComparisonOperator.EQ)
+                    .withAttributeValueList(new AttributeValue().withN(String.valueOf(geohashKey)));
+            }
+            keyConditions.push({ string: config.getGeoHashKeyColumn(), condition: geoHashKeyCondition });
+
+            // generate the geo hash range
+            let minRange = new AttributeValue().withN(Long.toString(range.getRangeMin()));
+            let maxRange = new AttributeValue().withN(Long.toString(range.getRangeMax()));
+
+            let geoHashCondition = new Condition().withComparisonOperator(ComparisonOperator.BETWEEN)
+                .withAttributeValueList(minRange, maxRange);
+            keyConditions.put(config.getGeoHashColumn(), geoHashCondition);
+
+            queryRequest.withKeyConditions(keyConditions)
+                .withIndexName(config.getGeoIndexName());
+            queryRequests.add(queryRequest);
+        }
+    }
+    return JSON.parse(JSON.stringify(queryRequests));
+}
 module.exports = {
-    generateGeoProperties
+    generateGeoProperties,
+    generateGeoQueries
 }
